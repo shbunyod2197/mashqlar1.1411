232dd6f9c7376f71851d5620693b65080273d143
shbunyod2197 <shbunyod2197@gmail.com>
2025-11-14 14:37:34 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+# 01-m
+matn = " python dasturlash  "
+print(len(matn))
+print(matn.lstrip())
+# 02-m
+matn = "hello world"
+print(matn, len(matn))
+print(matn.upper(), len(matn))
+# 03-m
+matn = " SALOM DUNYO 123 "
+print(matn)
+print(matn.lower(), matn.strip())
+# 04-m
+matn = "    o'zbekiston    "
+print(matn.rstrip())
+print(matn.capitalize())
+# 05-m
+matn = "   Toshkent   2025  "
+print(matn)
+print(matn.strip())
+print(matn.upper())
+# 06-m
+matn ="python3.11"
+print(matn)
+print(matn.title())
+print(len(matn))
+# 07-m
+matn = "ALGORITM VA DASTURLASH"
+print(matn)
+print(matn.upper())
+print(matn.lstrip())
+# 08-m
+matn = "   java vs python   "
+print(matn)
+print(matn.title())
+print(matn.strip())
